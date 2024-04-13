ad1d4dda8dda4d09154fc1c708ff24c350623ac1
Swapnil Jha <swapnil.strange0707@gmail.com>
2024-04-14 00:10:47 +0530

    cors added for deployement

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const PORT=process.env.PORT||3000;
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:['http://localhost:5173','https://namasteayodhya.vercel.app'],
     credentials: true,
 }))
 
